6f6c76ac21320238a67cbb190fb0ec806a9a993e
Kris Ringler <krisringler@kriss-air.attlocal.net>
2020-09-11 19:30:49 -0700

    all assets added, just need to get the video modal to work properly

--- src/components/stalloneVideo.js
+++ src/components/stalloneVideo.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+
+
 const StalloneVideo = () => (
   <section className="container section11">
     <div className="container video-placeholder-image mt-5 mb-5 text-light text-center p-5">
@@ -9,7 +11,9 @@ const StalloneVideo = () => (
       JOINS THE REVOLUTION
           </h2>
       <div className=" p-5">
-        <button className="btn pdf-button w-10 text-light" type="button">WATCH FULL VIDEO</button>
+        <button type="button" class="btn pdf-button text-light video-btn" data-toggle="modal" data-src="https://www.youtube.com/watch?v=FtjlQB4lRe0" data-target="#myModal">
+          WATCH FULL VIDEO
+</button>
       </div>
     </div>
 

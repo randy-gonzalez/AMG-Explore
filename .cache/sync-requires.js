f72d1b0b8af3e85638d2ad7f99fba012c7377fe7
Kris Ringler <krisringler@kriss-air.attlocal.net>
2020-09-22 20:03:04 -0700

    delete

---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/krisringler/shyft/AMG-Explore/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/krisringler/shyft/AMG-Explore/src/pages/404.js"))),
+  "component---src-pages-gas-vs-charcoal-js": hot(preferDefault(require("/Users/krisringler/shyft/AMG-Explore/src/pages/gas-vs-charcoal.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/krisringler/shyft/AMG-Explore/src/pages/index.js")))
+}
+
